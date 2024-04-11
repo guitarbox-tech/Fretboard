a1da61b5a08925e857de4c832c35cf9017d05386
Rodrigo <158451717+guitarbox-tech@users.noreply.github.com>
2024-04-11 14:44:04 +0000

    Undo y Redo OK cuando uso agregarFila, experimentos con eliminarFila (NO OK)

--- js/commands.js
+++ js/commands.js
@@ -22,9 +22,6 @@ class AddRowCommand extends Command {
     
         // Agregar una fila a la tabla
         agregarFila();
-    
-        // Agregar este comando al historial de comandos
-        commandHistory.add(this);
     }
     
 
@@ -130,55 +127,89 @@ class DeleteRowCommand extends Command {
 
         // Agregar una fila a la tabla
         eliminarFila(currentState.opacities, currentState.visibilities);
-
-        // Agregar este comando al historial de comandos
-        commandHistory.add(this);
     }
 
     undo() {
-        // Restaurar el estado anterior de la tabla
-        if (this.previousState) {
-            // Restaurar el HTML de la tabla
-            this.table.innerHTML = this.previousState.tablaHTML;
-
-            const tabla = document.getElementById("miTabla");
-            this.previousState.numFilasTabla = tabla.rows.length;
-
-            // Restaurar el número de filas
-            document.getElementById('numFilasButton').innerText = this.previousState.numFilasButton;
-
-            // Restaurar el número de columnas seleccionado
-            document.getElementById('numeroColumnas').value = this.previousState.numColumnas;
-
-            // Restaurar la nota seleccionada
-            document.getElementById('selectNota').value = this.previousState.notaSeleccionada;
-
-            // Restaurar el estado del botón Cambiar Estilo
-            document.getElementById('botonEstilo').disabled = this.previousState.estadoBotonEstilo;
+        // Obtener el último estado anterior del array
+        const previousState = this.previousStates.pop();
         
-            const botonesConfiguracion = document.getElementsByClassName('config-button');
-            for (let i = 0; i < botonesConfiguracion.length; i++) {
-                botonesConfiguracion[i].innerHTML = this.previousState.botonesConfiguracion[i].innerHTML;
-                botonesConfiguracion[i].className = this.previousState.botonesConfiguracion[i].className;
-                botonesConfiguracion[i].style.transform = this.previousState.botonesConfiguracion[i].translateY;
-            }
-
-            const pseudoElementos = this.previousState.pseudoElementos;
-            pseudoElementos.forEach(pseudoElemento => {
-                const selector = pseudoElemento.selector;
-                const styles = pseudoElemento.beforeStyles;
-                const elemento = document.querySelector(selector);
-                if (elemento) {
-                    Object.keys(styles).forEach(style => {
-                        elemento.style[style] = styles[style];
-                    });
-                }   
+        // Restaurar el estado anterior de la tabla
+        if (previousState) {
+            // Extraer los botones de configuración
+            const configButtons = Array.from(this.table.querySelectorAll('.config-button'));
+          
+            // Limpiar la tabla actual
+            this.table.innerHTML = '';
+            
+            // Guardar los botones de configuración y sus onclicks para restaurarlos más tarde
+            const savedButtons = [];
+            configButtons.forEach(button => {
+                const clonedButton = button.cloneNode(true); // Clonar el botón
+                clonedButton.setAttribute('translate', button.getAttribute('translate')); // Copiar el atributo 'translate'
+                savedButtons.push(clonedButton); // Agregar el botón clonado al array
             });
-        }
+            
+            // Crear un elemento temporal para contener el HTML de la tabla con tbody
+            const tempTable = document.createElement('table');
+            tempTable.innerHTML = previousState.tablaHTML;
+            
+            // Obtener el tbody si existe
+            const tbody = tempTable.querySelector('tbody');
+                         
+            // Si hay un tbody, mover sus hijos (los tr) a la tabla original
+            if (tbody) {
+                const tableRows = Array.from(tbody.children);
+                let buttonIndex = 0; // Inicializamos el índice del botón
+                let isFirstRow = true;
 
-        // Imprimir la información utilizada en undo
-        console.log("Información utilizada en deleteRowCommand.undo:", this.previousState);
+                tableRows.forEach(row => {
+                    const clonedRow = row.cloneNode(true); // Clonar la fila para evitar la eliminación del original
+                   
+                    
+                    // Obtener el botón correspondiente del array de botones guardados
+                    const button = savedButtons[buttonIndex];
+
+                    // Si hay un botón, agregarlo antes del primer td de la fila clonada
+                    if (button) {
+                        const firstCell = clonedRow.querySelector('td'); // Obtener la primera celda de la fila
+                        if (firstCell) {
+                            const clonedButton = button.cloneNode(true); // Clonar el botón
+                            clonedButton.setAttribute('translate', button.getAttribute('translate')); // Copiar el atributo 'translate'
+                    
+                            // Crear un nuevo onclick para el botón clonado
+                            clonedButton.onclick = function() {
+                                const nuevaNota = prompt("Afinación de la cuerda:");
+                                if (nuevaNota !== null) {
+                                    // Obtener la fila correspondiente al botón clicado
+                                    const row = this.closest('tr');
+                                    if (row) {
+                                        asignarAfinacion(row, nuevaNota);
+                                    }
+                                }
+                            };
+                    
+                            clonedRow.insertBefore(clonedButton, firstCell); // Insertar el botón antes de la primera celda
+                        }
+                    }
+                  
+                    var nuevaFila = clonedRow;
+                    this.table.appendChild(nuevaFila); // Agregar la fila clonada a la tabla original
+                           
+                    // Incrementar el índice del botón para la siguiente fila
+                    buttonIndex++;
+
+                    clicSvg();   
+                    modificarEstiloPseudoElementos();  
+                    
+                });  
+            }
+          
+            // Restaurar el número de filas
+            document.getElementById('numFilasButton').innerText = previousState.numFilasButton;            
+        }
     }
+    
+    
 }
 
 class ChangeNoteCommand extends Command {
@@ -213,10 +244,9 @@ class CommandHistory {
     // Agregar un comando a la pila de comandos deshechos
     add(command) {
         this.undoStack.push(command);
-        // Limpiar la pila de comandos rehacer
         this.redoStack = [];
-        console.log('Cantidad de comandos en la pila undo:', this.undoStack.length);
-        console.log('Cantidad de comandos en la pila redo:', this.redoStack.length);
+        console.log('ADD Cantidad de comandos en la pila undo:', this.undoStack.length);
+        console.log('ADD Cantidad de comandos en la pila redo:', this.redoStack.length);
     }
 
     // Deshacer el último comando
@@ -224,10 +254,9 @@ class CommandHistory {
         if (this.undoStack.length > 0) {
             const command = this.undoStack.pop();
             command.undo(command);
-            // Mover el comando deshecho a la pila de comandos rehacer
             this.redoStack.push(command);
-            console.log('Cantidad de comandos en la pila undo:', this.undoStack.length);
-            console.log('Cantidad de comandos en la pila redo:', this.redoStack.length);
+            console.log('UNDO Cantidad de comandos en la pila undo:', this.undoStack.length);
+            console.log('UNDO Cantidad de comandos en la pila redo:', this.redoStack.length);
         }
     }
 
@@ -236,10 +265,9 @@ class CommandHistory {
         if (this.redoStack.length > 0) {
             const command = this.redoStack.pop();
             command.execute();
-            // Mover el comando rehacer a la pila de comandos deshechos
-            this.undoStack.push(command);
-            console.log('Cantidad de comandos en la pila undo:', this.undoStack.length);
-            console.log('Cantidad de comandos en la pila redo:', this.redoStack.length);
+            this.undoStack.push(command);            
+            console.log('REDO Cantidad de comandos en la pila undo:', this.undoStack.length);
+            console.log('REDO Cantidad de comandos en la pila redo:', this.redoStack.length);
         }
     }
 }
@@ -268,3 +296,13 @@ const addRowCommand = new AddRowCommand(miTabla);
 const deleteRowCommand = new DeleteRowCommand(miTabla);
 const changeNoteCommand = new ChangeNoteCommand(miTabla, 2, "Nueva nota");
 
+function addRow() {
+    addRowCommand.execute(); // Ejecutar la acción para agregar una fila
+    commandHistory.add(addRowCommand); // Agregar el comando a la historia de comandos
+}
+
+
+function deleteRow() {
+    deleteRowCommand.execute(); // Ejecutar la acción para agregar una fila
+    commandHistory.add(deleteRowCommand); // Agregar el comando a la historia de comandos
+}
